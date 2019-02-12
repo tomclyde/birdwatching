57a56c1f27d7380cf8a7e28d3b6b4a26c063656d
Tom C <tclyde@hotmail.com>
2019-02-12 13:24:27 +0000

    MVP complete

---
+++ client/src/models/sightings.js
@@ -0,0 +1,43 @@
+const RequestHelper = require('../helpers/request_helper.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+const Sightings = function (url) {
+  this.url = url;
+  this.request = new RequestHelper(this.url);
+};
+
+Sightings.prototype.bindEvents = function () {
+  PubSub.subscribe('SightingView:sighting-delete-clicked', (evt) => {
+    this.deleteSighting(evt.detail);
+  });
+  PubSub.subscribe('SightingView:sighting-submitted', (evt) => {
+    this.postSighting(evt.detail);
+  })
+};
+
+Sightings.prototype.getData = function () {
+  this.request.get()
+    .then((sightings) => {
+      PubSub.publish('Sightings:data-loaded', sightings);
+    })
+    .catch(console.error);
+};
+
+Sightings.prototype.postSighting = function (sighting) {
+  const request = new Request(this.url);
+  request.post(sighting)
+    .then((sighting) => {
+      PubSub.publish('Sightings:data-loaded', sighting);
+    })
+    .catch(console.error);
+};
+
+Sightings.prototype.deleteSighting = function (sightingId) {
+  this.request.delete(sightingId)
+    .then((sightings) => {
+      PubSub.publish('Sightings:data-loaded', sightings);
+    })
+    .catch(console.error);
+};
+
+module.exports = Sightings;
